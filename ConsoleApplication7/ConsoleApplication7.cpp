d8cb8b005cde26935d1fc0a13c57561afddd7bc5
msi <msi@DESKTOP-Q39FBF1>
2023-11-23 21:27:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#define IS_EVEN(x) ((x) % 2 == 0)
+#define SUM_TO_N(n) ((n) * ((n) + 1) / 2)
+#define POWER(base, exponent) pow(base, exponent)
+
+using namespace std;
+
+/*
+void printStars(int n) {
+    if (n > 0) {
+        cout << "* ";
+        printStars(n - 1);
+    }
+    else {
+        cout << endl;
+    }
+}
+
+int main() {
+    int N;
+    cout << "enter num N: ";
+    cin >> N;
+
+    printStars(N);
+
+    return 0;
+}
+*/
+
+
+void printNumbers(int n) {
+    if (n > 0) {
+        printNumbers(n - 1);
+        cout << n << " ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "enter num n: ";
+    cin >> n;
+
+    printNumbers(n);
+    cout << endl;
+
+    return 0;
+}
